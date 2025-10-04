fd7f8d0ffad6a58074c6ca1214533868d24192da
Thiago Córdova <thicordova16@gmail.com>
2025-10-04 14:46:08 -0300

    fix: Animal routes

--- src/routes/animal.routes.ts
+++ src/routes/animal.routes.ts
@@ -13,12 +13,12 @@ import { upload } from "../middlewares/multer";
 
 const router = Router();
 
-router.get('/:id', find)
-router.post('/register', upload.array("midia", 10), authenticated(UserType.ONG), register);
-router.put('/:id', upload.array("midia", 10), authenticated(UserType.ONG), edit)
-router.delete('/:id', exclude)
-
 router.get('/', getAnimals)
 router.get('/my', authenticated(UserType.ONG), getMyAnimals);
+
+router.post('/register', upload.array("midia", 10), authenticated(UserType.ONG), register);
+router.get('/:id', find)
+router.put('/:id', upload.array("midia", 10), authenticated(UserType.ONG), edit)
+router.delete('/:id', exclude)
 
 export default router;
